ab92761fa512b84e4218b883a8edb8126baf1cf0
hime151 <hime151@gmail.com>
2018-07-31 18:39:05 -0500

    added 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello s3dovgin!");
+System.out.println("Hello s2dovgin");
+System.out.println("Hello s1dovgin")
     }
 }
