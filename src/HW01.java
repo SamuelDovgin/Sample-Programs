eb89f1598a9aa56b8dc4e4984bd6c9e910dd60c3
s3dovgin <s3dovgin@gmail.com>
2018-07-31 17:22:46 -0500

    added link to java AP Comp Sci Subset

--- src/HW01.java
+++ src/HW01.java
@@ -1,9 +1,11 @@
 public class HW01 {
 
+    //Java subset: https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap-computer-science-a-java-subset.pdf
     /**
      * Return the sum of the elements in array.
      */
     public static int sum(final int[] array) {
+
         return 0;
     }
 
