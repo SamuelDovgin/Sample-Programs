188817b21618aa26435879840377947e0feaf732
hime151 <hime151@gmail.com>
2018-07-31 18:24:27 -0500

    added more println

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello s3dovgin!");
+        System.out.println("Hello s2dovgin!");
+        System.out.println("Hello s1dovgin!");
     }
 }
